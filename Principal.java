c3d5a2307289f17f7757b2684d6b5dbb69b4a8ab
s2Alive <rodrigocorreiaba753@gmail.com>
2021-11-09 03:22:28 -0300

    v3.0

--- Principal.java
+++ Principal.java
@@ -3,6 +3,6 @@ import gui.MainPage;
 public class Principal{
     public static void main(String[] args) {
         
-        MainPage janelaListagem = new MainPage(); //danilao amorzinho de gustavo
+        MainPage danilo = new MainPage(); //<3
     }
 }
--- gui/MainPage.java
+++ gui/MainPage.java
@@ -1,5 +1,6 @@
 package gui;
 
+import javax.lang.model.util.ElementScanner14;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.TableColumnModel;
@@ -26,31 +27,34 @@ public class MainPage extends JFrame {
     /* Atributos --------------------------------------------------------- */
     private JButton botaoCadastrar;
     private JButton botaoVisualizar;
-    private JButton btnShowDisciplinas;
     private JLabel labelTorneios;
     private JComboBox caixaTorneios;
+    public int jogo;
 
     /* Construtores ----------------------------------------------------- */
     public MainPage() {
         super("Torneios de E-sports");
 
         // inicialização dos componentes
-        btnShowDisciplinas = new JButton("Mostrar disciplinas do aluno");
 
         labelTorneios = new JLabel("Campeonato:");
         // array de textos que será passado para o ComboBox
-        String[] signos = {"League of Legends","Counter-Strike: Global Offensive","Valorant","FIFA 2021","Clash Royale","Overwatch",
-                           "Among us","DOTA 2","Skyrim","DOOM Eternal","Pokemon","Legends of Runeterra"};
+        String[] signos = {"League of Legends","CS.GO","Valorant","FIFA 2021"};
         caixaTorneios = new JComboBox<String>(signos);
+        caixaTorneios.setSize(80, 30);
         botaoCadastrar = new JButton("Cadastrar Participante");
+        botaoCadastrar.setSize(50, 30);
+        botaoCadastrar.addActionListener(new EventoCadastrar());
         botaoVisualizar = new JButton("Vizualizar Equipe e Participantes");
+        botaoVisualizar.setSize(50, 30);
 
         // definição dos layouts
         JPanel panel = new JPanel(new BorderLayout(10, 10)); // espaçamento de 5px entre os componentes
         panel.setBorder(new EmptyBorder(50, 30, 50, 30)); // uma borda para afastar os compoentes da janela
-        panel.add(caixaTorneios, BorderLayout.NORTH);
-        panel.add(botaoCadastrar, BorderLayout.CENTER);
-        panel.add(botaoVisualizar, BorderLayout.SOUTH);
+        panel.add(labelTorneios, BorderLayout.NORTH);
+        panel.add(caixaTorneios, BorderLayout.SOUTH);
+        panel.add(botaoCadastrar, BorderLayout.WEST);
+        panel.add(botaoVisualizar, BorderLayout.EAST);
         add(panel); // coloco o painel dentro da janela
 
         // configuração da janela
@@ -64,5 +68,17 @@ public class MainPage extends JFrame {
     /* Métodos ------------------------------------------------------------- */
 
     /* Classes internas ---------------------------------------------------- */
+    private class EventoCadastrar implements ActionListener {
+        public void actionPerformed(ActionEvent e) { // o método invocado quando o btn cadastrar for pressionado
+            if (caixaTorneios.getSelectedItem() == "League of Legends"){System.out.println("lolzim");
+
+            }   else if (caixaTorneios.getSelectedItem() == "CS.GO"){System.out.println("cs");
+
+                }   else if (caixaTorneios.getSelectedItem() == "Valorant"){System.out.println("valorant");
+
+                    }else {System.out.println("fifa");}
+            Cadastro janelaCadastro = new Cadastro();
+        }
+    }
 
 }
