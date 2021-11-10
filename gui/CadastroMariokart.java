74519135e09b0c46711fa054497d228e94d46ef2
Mxrlla <marllasousar@gmail.com>
2021-11-09 22:23:15 -0300

    ok

--- gui/CadastroMariokart.java
+++ gui/CadastroMariokart.java
@@ -25,7 +25,7 @@ public class CadastroMariokart extends JFrame {
     public CadastroMariokart() {
         super("Cadastro de Equipe");
 
-        label CadastroMariokart = new JLabel("JOGADOR 1:")
+        label CadastroMariokart = new JLabel("JOGADOR 1");
 
         // inicialização dos cadastros J1
         labeltxt = new JLabel("Nome:");
@@ -58,6 +58,50 @@ public class CadastroMariokart extends JFrame {
         panel.add(txtNome, BorderLayout.CENTER);
         panel.add(txtCpf, BorderLayout.CENTER);
         panel.add(labelpersonagens, BorderLayout.SOUTH);
+         add(panel);
+    
+
+          // configuração da janela
+          setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+          setResizable(true); // impede o redimensionamento da janela
+          setLocation(600, 300);
+          pack(); // define o tamanho da janela (menor possível para caber o conteúdo)
+          setVisible(true);
+    }
+}
+         label CadastroMariokart = new JLabel("JOGADOR 2");
+
+         // inicialização dos cadastros J1
+         labeltxt = new JLabel("Nome:");
+         txtNome = new JTextField(50);
+         private boolean validacaoSalvar(){
+            // VALIDAÇÃO DO CAMPO NOME
+            if(txtNome.getText().length() == 0){ // se o campo 'nome' está vazio
+                JOptionPane.showMessageDialog(this, "O campo 'nome' deve estar preenchido!", "Erro de validação",JOptionPane.WARNING_MESSAGE);
+                return false; 
+            }
+            
+            // VALIDAÇÃO DO CAMPO CPF
+         String cpf = txtCpf.getText(); // obter o cpf completo digitado
+           cpf = cpf.replace(".", "");
+           cpf = cpf.replace("-", "");
+           cpf = cpf.replace(" ", "");
+         if(cpf.length() < 11){
+            JOptionPane.showMessageDialog(this, "O campo 'cpf' deve ter 11 números!", "Erro de validação",JOptionPane.WARNING_MESSAGE);
+            return false; }
+
+           // Escolher o persongaem do jogador
+           labelpersonagens= new JLabel(" Escolha o seu personagem:");
+            String[] personagens = {"Mário","Princesa Peach","Luigi","Princesa Daisy","Toad","Toadette","Yoshi", "Birdo"};
+            caixapersonagens = new JComboBox<String>(personagens);
+            caixapersonagensSize(80, 30); }
+            
+          JPanel panel = new JPanel(new BorderLayout(10, 10)); // espaçamento de 5px entre os componentes
+        panel.setBorder(new EmptyBorder(50, 30, 50, 30)); // uma borda para afastar os compoentes da janela
+        panel.add(labeltxt, BorderLayout.NORTH);
+        panel.add(txtNome, BorderLayout.CENTER);
+        panel.add(txtCpf, BorderLayout.CENTER);
+        panel.add(labelpersonagens, BorderLayout.SOUTH);
 
         add(panel);
     
@@ -66,7 +110,54 @@ public class CadastroMariokart extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setResizable(true); // impede o redimensionamento da janela
         setLocation(600, 300);
+     
         pack(); // define o tamanho da janela (menor possível para caber o conteúdo)
         setVisible(true);
     }
+ }
+    label CadastroMariokart = new JLabel("JOGADOR 3");
+
+    // inicialização dos cadastros J1
+    labeltxt = new JLabel("Nome:");
+    txtNome = new JTextField(50);
+    private boolean validacaoSalvar(){
+       // VALIDAÇÃO DO CAMPO NOME
+       if(txtNome.getText().length() == 0){ // se o campo 'nome' está vazio
+           JOptionPane.showMessageDialog(this, "O campo 'nome' deve estar preenchido!", "Erro de validação",JOptionPane.WARNING_MESSAGE);
+           return false; 
+       }
+       
+       // VALIDAÇÃO DO CAMPO CPF
+    String cpf = txtCpf.getText(); // obter o cpf completo digitado
+      cpf = cpf.replace(".", "");
+      cpf = cpf.replace("-", "");
+      cpf = cpf.replace(" ", "");
+    if(cpf.length() < 11){
+       JOptionPane.showMessageDialog(this, "O campo 'cpf' deve ter 11 números!", "Erro de validação",JOptionPane.WARNING_MESSAGE);
+       return false; }
+
+      // Escolher o persongaem do jogador
+      labelpersonagens= new JLabel(" Escolha o seu personagem:");
+       String[] personagens = {"Mário","Princesa Peach","Luigi","Princesa Daisy","Toad","Toadette","Yoshi", "Birdo"};
+       caixapersonagens = new JComboBox<String>(personagens);
+       caixapersonagensSize(80, 30); }
+       
+     JPanel panel = new JPanel(new BorderLayout(10, 10)); // espaçamento de 5px entre os componentes
+     panel.setBorder(new EmptyBorder(50, 30, 50, 30)); // uma borda para afastar os compoentes da janela
+     panel.add(labeltxt, BorderLayout.NORTH);
+     panel.add(txtNome, BorderLayout.CENTER);
+     panel.add(txtCpf, BorderLayout.CENTER);
+     panel.add(labelpersonagens, BorderLayout.SOUTH);
+
+     add(panel);
+
+
+     // configuração da janela
+     setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+     setResizable(true); // impede o redimensionamento da janela
+     setLocation(600, 300);
+      pack(); // define o tamanho da janela (menor possível para caber o conteúdo)
+     setVisible(true);
+    }
+
 }
