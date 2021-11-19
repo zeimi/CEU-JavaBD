41fea9ee2dfee2befa4aeb86d395b550ed9518d6
Zeimi <contato.brunodurao@gmail.com>
2021-11-19 18:31:57 -0300

    unused

--- Principal.java
+++ Principal.java
@@ -1,6 +1,7 @@
 import gui.MainPage;
 
 public class Principal{
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         
         MainPage CEU = new MainPage(); //<3 dan
